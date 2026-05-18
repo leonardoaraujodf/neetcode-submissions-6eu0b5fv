d3bb6e1a6f9773a9638f79befb0d429b5ce8296f
Leonardo Amorim <leonardoaraujodf@gmail.com>
2026-05-18 08:22:04 -0300

    Add: find-minimum-in-rotated-sorted-array - submission-3

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-3.go
@@ -0,0 +1,17 @@
+func findMin(nums []int) int {
+	left, right := 0, len(nums)-1
+	for left < right {
+		if nums[left] < nums[right] {
+			return nums[left]
+		}
+		mid := left + (right - left)/2
+		
+		if nums[mid] > nums[right] {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+
+	return nums[left]
+}
