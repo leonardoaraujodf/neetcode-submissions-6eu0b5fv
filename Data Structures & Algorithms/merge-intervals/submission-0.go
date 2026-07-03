c5da6e51055c89a0516a9c9fbc45fb2074418d0b
Leonardo Amorim <leonardoaraujodf@gmail.com>
2026-07-03 11:01:49 -0300

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.go
@@ -0,0 +1,15 @@
+func merge(intervals [][]int) [][]int {
+    sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	merged := make([][]int, 0)
+	for _, interval := range intervals {
+		if len(merged) == 0 || interval[0] > merged[len(merged)-1][1] {
+			merged = append(merged, interval)
+		} else {
+			merged[len(merged)-1][1] = max(merged[len(merged)-1][1], interval[1])
+		}
+	}
+	return merged
+}
